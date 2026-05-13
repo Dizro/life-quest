8a64acdbe18d388ec8c1caeb7df83797e6100f29
Дмитрий Дубина <dld1@tpu.ru>
2026-05-14 04:58:30 +0700

    fix: CORS allow dizro.github.io origin explicitly

--- lifequest-backend/app/main.py
+++ lifequest-backend/app/main.py
@@ -12,7 +12,12 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://dizro.github.io",
+        "http://localhost:5173",
+        "http://localhost:5174",
+        "http://127.0.0.1:5173",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
